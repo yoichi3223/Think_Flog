c7f39eee662875600b7b1bbbd11f91da846de658
yoichi3223 <yoichi3223@gmail.com>
2020-09-26 11:27:34 +0000

    production.rb modifiled3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,14 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => "gmail.com",
-      :user_name => 'yoichi3223@gmail.com',
-      :password => "fryraubsiilsybxb", #2段階認証パスワード
-      :authentication => :plain,
-      :enable_starttls_auto => true
-  }
+  
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :user_name => 'yoichi3223@gmail.com',
+      :password => "fryraubsiilsybxb", #2段階認証パスワード
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 end
