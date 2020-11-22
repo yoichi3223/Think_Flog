eb6ae223ace0006c9b6fe0c4996c14f460aeb4c6
yoichi3223 <yoichi3223@gmail.com>
2020-11-22 13:47:58 +0000

    index list-modal add

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,7 @@
 class TopController < ApplicationController
   	def index
         @lists = List.where(user: current_user).order("created_at ASC")
+        @list = List.new
+        @genre = Genre.where(user: current_user,status: :true)
     end
 end
